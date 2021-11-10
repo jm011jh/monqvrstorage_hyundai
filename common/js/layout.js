e67ae3f1b666da2d69cf763e4acb68e699a16f85
jm011jh <jm011jh@gmail.com>
2021-11-10 17:05:59 +0900

    SJM ham added

--- common/js/layout.js
+++ common/js/layout.js
@@ -3,11 +3,20 @@ $(document).ready(function(){
     const btnPopupOpen2 = document.getElementById('btnPopupOpen2')
     const btnPopupClose1 = document.getElementById('btnPopupClose1')
     const btnPopupClose2 = document.getElementById('btnPopupClose2')
+    const langBox = document.getElementById('langBox')
+    const langBtn = document.getElementById('langBtn')
+    const btnHam = document.getElementById('btnHam')
+    const navPopup = document.getElementById('navPopup')
+    const navBG = document.getElementById('navBG')
+    const navCont = document.getElementById('navCont')
     var popups = document.getElementsByClassName('popup')
     var popupSW = false;
+    var langSW = false;
+    var hamSW = false;
     function popupFn(e){
         var targetID = String(e.target.id);
         var i = Number(targetID.charAt(targetID.length-1)) - 1
+        
         if(popupSW){
             popups[i].classList.remove('on')
         }else{
@@ -31,4 +40,48 @@ $(document).ready(function(){
         $('.popup').removeClass('on')
         popupSW = false;
     })
+
+    function langBoxFn(e){
+        if(langSW){
+            langBox.classList.remove('on')
+        }else{
+            langBox.classList.add('on')
+        }
+        langSW = !langSW;
+    }
+    langBtn.addEventListener('click',function(){
+        langBoxFn()
+    })
+
+    function navPopupFn(e){
+        if(hamSW){
+            navCont.classList.remove('on')
+            setTimeout(()=>{
+                navPopup.classList.remove('on')
+            },310)
+        }else{
+            navPopup.classList.add('on')
+            setTimeout(()=>{
+                navCont.classList.add('on')
+            },10)
+        }
+        setTimeout(()=>{
+            hamSW = !hamSW
+        },320)
+    }
+    btnHam.addEventListener('click',function(){
+        navPopupFn()
+    })
+    navBG.addEventListener('click',function(){
+        navPopupFn()
+    })
+    window.onresize = function(e){
+        setTimeout(()=>{
+        if(hamSW==true){
+            hamSW = false;
+            navPopup.classList.remove('on')
+            navCont.classList.remove('on')
+            }
+        },10)
+    }
 })
